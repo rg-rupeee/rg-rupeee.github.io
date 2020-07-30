6781f23ceef67ef65b6282aba5adf235ec8821ba
rg-rupeee <62208754+rg-rupeee@users.noreply.github.com>
2020-07-30 22:27:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
       scores[activePlayer];
 
     //3. Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner";
       document.querySelector(".dice").style.display = "none";
       document
